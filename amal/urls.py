785135f7da8987bb2b228ed4a22294446b1b1a2a
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2019-09-01 19:37:37 +0545

    add product rags details for product scraping

--- amal/urls.py
+++ amal/urls.py
@@ -4,6 +4,7 @@ This file contains urls for
 scraping data from Amazom and Aliaba
 
 """
+import re
 
 AMAZON_URL = "https://www.amazon.com/"
 ALIBABA_URL = "https://www.alibaba.com/"
@@ -14,3 +15,13 @@ ALIBABA_SEARCH_TAB = "#J_SC_header > header > div.sc-hd-row.sc-hd-main > div.sc-
 
 AMAZON_ITEMS_PAGE = "https://www.amazon.com/dp/"
 ALIBABA_ITEMS_PAGE = "https://www.alibaba.com/product-detail/"
+
+#for scraping item code
+
+AMAZON_ITEM_CODE_TAGS = {"tags": ["data-asin"],
+                        "value": "data-asin",
+                        "element": "div"}
+ALIBABA_ITEM_CODE_TAGS = {"tags": ["data-domdot","data-spm-anchor-id"],
+                            "value": "href",
+                            "element":"a",
+                            "regex": re.compile(f"^{ALIBABA_ITEMS_PAGE}.*$")}
