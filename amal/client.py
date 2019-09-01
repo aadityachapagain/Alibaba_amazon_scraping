56e58c8abc9e16298a3da97b92439c729b22827f
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2019-09-01 21:20:35 +0545

    add scraping code for item_codes

--- amal/client.py
+++ amal/client.py
@@ -2,7 +2,7 @@
 from abc import ABCMeta, abstractmethod
 
 # import modules
-from amal.urls import ALIBABA_URL, AMAZON_URL, ALIBABA_SEARCH_TAB, AMAZON_ITEMS_PAGE, ALIBABA_SEARCH_TAB
+from amal.urls import ALIBABA_URL, AMAZON_URL, ALIBABA_SEARCH_TAB, AMAZON_ITEMS_PAGE, AMAZON_SEARCH_TAB
 
 class Client(metaclass= ABCMeta):
 
@@ -15,30 +15,38 @@ class Client(metaclass= ABCMeta):
         pass
 
     @abstractmethod
-    def get_item_code(self, item_name):
+    def get_item_code(self):
         pass
 
 
 class AmazonClient(Client):
 
-    def __init__(self):
+    def __init__(self, item_name):
         super().__init__()
+        self.url = AMAZON_URL
+        self.item_name = item_name
+        self.class_ = "AMAZON"
+        self.SEARCH_TAB = AMAZON_SEARCH_TAB
 
     
     def scrape(self):
         return super().scrape()
 
-    def get_item_code(self, item_name):
-        return super().get_item_code(item_name)
+    def get_item_code(self):
+        self._get_item_code()
 
 
 class AlibabaClient(Client):
 
-    def __init__(self):
+    def __init__(self, item_name):
         super().__init__()
+        self.url = ALIBABA_URL
+        self.item_name = item_name
+        self.class_ = "ALIBABA"
+        self.SEARCH_TAB = ALIBABA_SEARCH_TAB
 
     def scrape(self):
         return super().scrape()
 
-    def get_item_code(self, item_name):
-        return super().get_item_code(item_name)
+    def get_item_code(self):
+        self._get_item_code()
--- amal/scrape.py
+++ amal/scrape.py
@@ -1,11 +1,39 @@
+from abc import ABCMeta, abstractmethod
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 
+# put all the Dirty works here
 
-class Scraper(object):
-    pass
+class Scraper(metaclass=ABCMeta):
 
+    def __init__(self):
+        self._item_codes = []
+        self.options = Options()  
+        self.options.add_argument("--headless")  
 
+    @abstractmethod
+    def _get_item_code(self):
+        browser = webdriver.Firefox(firefox_options=options)
+        browser.get(self.url)
+        elem = browser.find_element_by_css_selector(self.SEARCH_TAB)
+        elem.send_keys(self.item_name)
+        elem.send_keys(Keys.RETURN)
+        return browser
+
+class AmazonScraper(Scraper):
+
+    def __init__(self):
+        super().__init__()
+
+    def _get_item_code(self):
+        browser = super()._get_item_code()
+
+# concept of worker
+# 1 worker is one browser instance of slenium webdriver to paralalize the scraping process
+# Scraper will initiate worker with multiprocessing library
 class Worker(object):
-    pass
+    
+    def __init__(self, url):
+        self._url = url
