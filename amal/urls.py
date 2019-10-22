907b7c47534af884c2a64f0da2d8499289f34aa3
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2019-10-22 23:22:15 +0545

    modify: amazon xpath urls contained inside class

--- amal/urls.py
+++ amal/urls.py
@@ -27,7 +27,12 @@ ALIBABA_ITEM_CODE_TAGS = {"tags": ["data-domdot","data-spm-anchor-id"],
                             "regex": re.compile(f"^{ALIBABA_ITEMS_PAGE}.*$")}
 
 # Amazon item page x_path for details
+class AmazonItemsPaths:
+    PRODUCT_X_PATH = '//*[@id="productTitle"]'
+    PRICE_X_PATH = '//*[@id="priceblock_ourprice"]'
+    PRODUCT_PRICE_RATE_X_PATH = '/html/body/div[2]/div[1]/div[6]/div[5]/div[3]/div[9]/div/div/table/tbody/tr[1]/td[2]/span[2]'
+    PRODUCT_INFO_X_PATH = '//*[@id="feature-bullets"]/ul'
 
-AMAZON_PRODUCT_X_PATH = '//*[@id="productTitle"]'
-AMAZON_PRICE_X_PATH = '//*[@id="priceblock_ourprice"]'
-AMAZON_PRODUCT_PRICE_RATE_X_PATH = '/html/body/div[2]/div[1]/div[6]/div[5]/div[3]/div[9]/div/div/table/tbody/tr[1]/td[2]/span[2]'
+# Alibaba item page x_path for details
+class AlibabaItemsPaths:
+    pass
