5f7dc54d8f830903f0708bb032fe92716fb17fbb
aaditya <aaditya@fusemachines.com>
2019-10-22 20:07:57 +0545

    add x_paths for the amazom products details

--- amal/urls.py
+++ amal/urls.py
@@ -25,3 +25,9 @@ ALIBABA_ITEM_CODE_TAGS = {"tags": ["data-domdot","data-spm-anchor-id"],
                             "value": "href",
                             "element":"a",
                             "regex": re.compile(f"^{ALIBABA_ITEMS_PAGE}.*$")}
+
+# Amazon item page x_path for details
+
+AMAZON_PRODUCT_X_PATH = '//*[@id="productTitle"]'
+AMAZON_PRICE_X_PATH = '//*[@id="priceblock_ourprice"]'
+AMAZON_PRODUCT_PRICE_RATE_X_PATH = '/html/body/div[2]/div[1]/div[6]/div[5]/div[3]/div[9]/div/div/table/tbody/tr[1]/td[2]/span[2]'
