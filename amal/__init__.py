06cd7fecb31da466879ef70960235a3174966c4c
aaditya <aaditya@fusemachines.com>
2019-10-25 10:42:36 +0545

    add mixins class for amazonscraper and alibaba scraper

--- amal/__init__.py
+++ amal/__init__.py
@@ -17,3 +17,27 @@
 
 __version__ = '1.1.0'
 __author__ = 'Aaditya Chapagain'
+
+from amal.client import AlibabaClient, AmazonClient
+from amal.scrape import AlibabaScraper, AmazonScraper
+
+
+
+class Amazon(AmazonClient, AmazonScraper):
+    """
+    Amazon class is  mixins of both AmazonClient and AmazonScraper,
+    This class is becoming to scrape both items imformation and Asin codes
+    """
+
+    def __init__(self, item_name):
+        super().__init__(item_name)
+
+
+class Alibaba(AlibabaClient, AlibabaScraper):
+    """
+    Alibaba class is mixins of both AlibabClient and Alibaba Scraper,
+    This class is becoming to scrape both items imformation and links from amazon
+    """
+
+    def __init__(self, item_name):
+        super().__init__(item_name)
