716ba935edf8882deccf84e4dd595d3082101d90
aaditya <aaditya@fusemachines.com>
2019-10-25 11:26:40 +0545

    add selenioum driver options import in scrape.py

--- amal/scrape.py
+++ amal/scrape.py
@@ -4,6 +4,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
